e4a48aafcc08dfd419bf8aa874085f4780be9b96
Jesse Segall <jsegall7@gmail.com>
2024-01-17 15:20:02 -0300

    Added BC as a security provider

--- src/main/java/com/jessesegall/Main.java
+++ src/main/java/com/jessesegall/Main.java
@@ -1,8 +1,14 @@
 package com.jessesegall;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
 import java.util.Scanner;
 
 public class Main {
+    static {
+        Security.addProvider(new BouncyCastleProvider()); // Register BC as a sec provider
+    }
     public static void main(String[] args) {
 
     }
