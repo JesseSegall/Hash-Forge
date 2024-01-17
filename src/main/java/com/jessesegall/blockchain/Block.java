2091ec29b5b15b30d6c74d83323d997becce52b2
Jesse Segall <jsegall7@gmail.com>
2024-01-17 15:23:41 -0300

    Moved Sha function to utils.

--- src/main/java/com/jessesegall/blockchain/Block.java
+++ src/main/java/com/jessesegall/blockchain/Block.java
@@ -1,8 +1,5 @@
 package com.jessesegall.blockchain;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.List;
 
@@ -26,34 +23,7 @@ public class Block {
 
     public String calculateHash() {
         String input = previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + transactions.toString();
-        return applySha256(input);
-    }
-
-
-
-    // TODO move to util class later
-    public static String applySha256(String input) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
-            // Apply hash to input
-
-            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-
-            //Hash as hexadecimal
-
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
+        return BlockchainUtils.applySha256(input);
     }
 
 
