d8a0ac8d30c08a228ad2345a2754ca061db718fd
Jesse Segall <jsegall7@gmail.com>
2024-01-17 15:23:06 -0300

    Some initial work on Blockchain class.

--- src/main/java/com/jessesegall/blockchain/Blockchain.java
+++ src/main/java/com/jessesegall/blockchain/Blockchain.java
@@ -1,8 +1,33 @@
 package com.jessesegall.blockchain;
+import com.jessesegall.consensus.ProofOfWork;
 
+import java.util.ArrayList;
+import java.util.List;
 // Manages the chain of blocks.
 public class Blockchain {
+    private static Blockchain instance;
+    private List<Block> chain;
+    private ProofOfWork proofOfWork;
 
+    public Blockchain(int difficulty){
+        this.chain = new ArrayList<>();
+        this.proofOfWork = new ProofOfWork(difficulty);
+
+        createGenesisBlock();
+    }
+
+    public static Blockchain getInstance(int difficulty) {
+        if (instance == null) {
+            instance =  new Blockchain(difficulty);
+        }
+        return instance;
+    }
+
+    private void createGenesisBlock() {
+        Block genesis = new Block("0", new ArrayList<>());
+        proofOfWork.mine(genesis);
+        chain.add(genesis);
+    }
 
 
 }
