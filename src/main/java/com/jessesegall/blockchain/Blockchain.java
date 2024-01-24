ec6bd2e54c884d5af7c75840468a31ef95f7be66
Jesse <jsegall7@gmail.com>
2024-01-24 09:40:42 -0500

    Completed methods to validate and add blocks to the chain.

--- src/main/java/com/jessesegall/blockchain/Blockchain.java
+++ src/main/java/com/jessesegall/blockchain/Blockchain.java
@@ -1,24 +1,26 @@
 package com.jessesegall.blockchain;
+
 import com.jessesegall.consensus.ProofOfWork;
 
 import java.util.ArrayList;
 import java.util.List;
+
 // Manages the chain of blocks.
 public class Blockchain {
     private static Blockchain instance;
     private List<Block> chain;
     private ProofOfWork proofOfWork;
+    private UTXOManager utxoManager = new UTXOManager();
 
-    public Blockchain(int difficulty){
+    public Blockchain(int difficulty) {
         this.chain = new ArrayList<>();
         this.proofOfWork = new ProofOfWork(difficulty);
-
         createGenesisBlock();
     }
 
     public static Blockchain getInstance(int difficulty) {
         if (instance == null) {
-            instance =  new Blockchain(difficulty);
+            instance = new Blockchain(difficulty);
         }
         return instance;
     }
@@ -29,5 +31,58 @@ public class Blockchain {
         chain.add(genesis);
     }
 
+    public boolean addBlock(Block block) {
+        if (validateNewBlock(block, getLastBlock())) {
+            chain.add(block);
+            updateUTXOSet(block);
+            return true;
+        }
+        return false;
+    }
 
+    private Block getLastBlock() {
+        return !chain.isEmpty() ? chain.get(chain.size() - 1) : null;
+    }
+
+
+    private boolean validateNewBlock(Block newBlock, Block previousBlock) {
+        if (previousBlock != null && !newBlock.getPreviousHash().equals(previousBlock.getHash())) {
+            return false;
+        }
+        return newBlock.validateTransactions(utxoManager) && proofOfWork.validate(newBlock);
+    }
+
+    private void updateUTXOSet(Block block) {
+        for (Transaction transaction : block.getTransactions()) {
+            for (TransactionInput input : transaction.getInputs()) {
+                utxoManager.removeUTXO(input.getUTXO().getId());
+            }
+            for (TransactionOutput output : transaction.getOutputs()) {
+                utxoManager.addUTXO(output);
+            }
+        }
+    }
+
+    public boolean validateBlockchain() {
+        Block currentBlock;
+        Block previousBlock;
+        for (int i = 1; i < chain.size(); i++) {
+            currentBlock = chain.get(i);
+            previousBlock = chain.get(i - 1);
+
+            if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
+                return false;
+            }
+
+            if (!previousBlock.getHash().equals(currentBlock.getPreviousHash())) {
+                return false;
+            }
+
+            if (!proofOfWork.validate(currentBlock)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
 }
