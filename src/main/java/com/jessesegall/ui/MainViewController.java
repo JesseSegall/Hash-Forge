582c928cd50ab147dc28efbf557b12f5767a2e01
Jesse <jsegall7@gmail.com>
2024-03-07 13:05:15 -0500

    Syle changes and add fields to init objects for UI integration

--- src/main/java/com/jessesegall/ui/MainViewController.java
+++ src/main/java/com/jessesegall/ui/MainViewController.java
@@ -1,23 +1,32 @@
 package com.jessesegall.ui;
 
+import com.jessesegall.blockchain.*;
+import com.jessesegall.network.P2PNetwork;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
-
-import javafx.fxml.FXMLLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainViewController {
+    private Blockchain blockchain;
+    private UTXOManager utxoManager;
+    private Wallet wallet;
+    private TransactionPool transactionPool;
+    private P2PNetwork p2pNetwork;
+    private Miner miner;
 
     @FXML
     private StackPane contentArea;
 
     @FXML
-    public void initialize() {
-        showOverview(); // Default view
-    }
+    private Label balanceLabel;
+
 
     @FXML
     private void showOverview() {
@@ -39,9 +48,40 @@ public class MainViewController {
         loadView("Mine.fxml");
     }
 
+
+    @FXML
+    public void initialize() {
+        // Initialize the instances
+        blockchain = new Blockchain(7);
+        utxoManager = new UTXOManager();
+        transactionPool = new TransactionPool();
+
+        List<String> knownNodes = new ArrayList<>();
+        knownNodes.add("localhost:8081");
+
+        p2pNetwork = new P2PNetwork(8080, blockchain, knownNodes);
+
+        wallet = new Wallet(blockchain, p2pNetwork);
+        miner = new Miner(blockchain, utxoManager, wallet, p2pNetwork);
+
+        // Start the P2PNetwork
+        p2pNetwork.start();
+
+        // Show the initial wallet balance
+        updateWalletBalance();
+
+        showOverview();
+    }
+
+    private void updateWalletBalance() {
+        float balance = wallet.getBalance();
+        //TODO: update the balance label
+        balanceLabel.setText("Balance: " + balance);
+    }
+
     private void loadView(String fxmlFile) {
         try {
-            // Adjusted the path to include the 'fxml/' directory
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFile));
             Node view = loader.load();
             contentArea.getChildren().setAll(view);
